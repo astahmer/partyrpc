16ef05aa2320914b7d71b98564c1079807e6c3fa
ayoubqrt <ayoub.mstk@gmail.com>
2023-12-07 23:33:58 +0100

    types

--- examples/basic/src/xyz.ts
+++ examples/basic/src/xyz.ts
@@ -3,7 +3,7 @@ import type * as Party from "partykit/server";
 export default class Server implements Party.Server {
   constructor(readonly party: Party.Party) {}
 
-  onRequest(_req) {
+  onRequest(_req: Party.Request) {
     return new Response("Hello from xyz");
   }
 
